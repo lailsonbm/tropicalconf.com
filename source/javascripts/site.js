7c5c86aebab4211f293cc54306b87757e649d280
filipeafns <filipe.afns@gmail.com>
2017-07-28 15:17:23 -0300

    updates javascript files

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
-// This is where it all goes :)
+var currentTime = new Date().getHours();
+if (6 <= currentTime && currentTime < 20) {
+    if (document.body) {
+        document.body.background-color = "$white";
+    }
+}
+else {
+    if (document.body) {
+        document.body.background = "$dark-blue";
+    }
+}
