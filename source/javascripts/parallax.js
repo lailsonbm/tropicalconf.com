a2e32cb22eb64afa3d1b68d45549e80ad42796e6
Bruno Leite <bruno@guava.com.br>
2017-10-03 17:38:15 -0300

    Adds the parallax effect on the speakers section

--- source/javascripts/parallax.js
+++ source/javascripts/parallax.js
@@ -1,14 +1,9 @@
 function Parallax() {
 	scrollPos = $(this).scrollTop();
 
-	$('body').css({
+	$('body, .grid.of-schedule').css({
 		'background-position' : '0% ' + (-scrollPos/4)+"px"
 	});
-
-	$('.content-right').css({
-		'background-position' : '0% ' + (-scrollPos/2)+"px"
-	});
-
 }
 $(function() {
 
